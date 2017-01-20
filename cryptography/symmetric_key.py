4e1f998bd39dfecef0395b6854b2760ff718cae8
Carl Shan <shan.carl@gmail.com>
2017-01-20 14:31:56 -0800

    Adding simple symmetric key encryption

--- cryptography/symmetric_key.py
+++ cryptography/symmetric_key.py
@@ -1,5 +1,22 @@
 """
 Simple implementation of symmetric-key encryption
+Relies on cryptography's Fernnet
+Docs: https://cryptography.io/en/latest/fernet/
 """
+from cryptography.fernet import Fernet
 
+secret_key = Fernet.generate_key()
 
+f = Fernet(secret_key)
+# Binds input to raw_input to handle backwards compability between Python 2.x
+# and 3.x
+try:
+    input = raw_input
+except NameError:
+    pass
+
+message = input('Type a message you want to encrypt here: \n')
+
+encrypted = f.encrypt(message)
+print("Your message was: {}".format(message))
+print("This became encrypted to: \n {}".format(encrypted))
